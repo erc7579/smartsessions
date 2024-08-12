fd01a47a5ece4321340443f342304624f0641658
zeroknots <zeroknots@protonmail.com>
2024-08-12 13:21:40 +0700

    performance improvement

--- contracts/lib/PolicyLib.sol
+++ contracts/lib/PolicyLib.sol
@@ -181,10 +181,9 @@ library PolicyLib {
             ActionId actionId = actionPolicyData.actionId;
             SessionId sessionId = signerId.toSessionId(actionId, smartAccount);
             if (
-                $self.enabledActionIds[signerId].contains(smartAccount, ActionId.unwrap(actionId))
-                    && $self.actionPolicies[actionId].areEnabled(
-                        signerId, sessionId, smartAccount, actionPolicyData.actionPolicies
-                    )
+                $self.actionPolicies[actionId].areEnabled(
+                    signerId, sessionId, smartAccount, actionPolicyData.actionPolicies
+                )
             ) actionsProperlyEnabled++;
         }
         if (actionsProperlyEnabled == 0) return false;
