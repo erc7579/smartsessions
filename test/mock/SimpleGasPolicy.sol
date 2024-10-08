4cdc39420f90039c6f3a48d758531a2ff2a0fe06
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2024-10-08 13:13:30 +0300

    simple gas policy

--- test/mock/SimpleGasPolicy.sol
+++ test/mock/SimpleGasPolicy.sol
@@ -11,19 +11,32 @@ contract SimpleGasPolicy is IUserOpPolicy {
         uint256 gasUsed;
     }
 
-    mapping(address msgSender => mapping(address opSender => uint256)) public usedIds;
     mapping(ConfigId id => mapping(address msgSender => mapping(address userOpSender => GasLimitConfig))) public
         gasLimitConfigs;
 
     function checkUserOpPolicy(ConfigId id, PackedUserOperation calldata userOp) external returns (uint256) {
         GasLimitConfig storage config = gasLimitConfigs[id][msg.sender][userOp.sender];
-        if (config.gasLimit == 0) {
-            revert("GasLimitPolicy: policy not installed");
+        require(config.gasLimit > 0, PolicyNotInitialized(id, msg.sender, userOp.sender));
+        
+        uint256 totalUserOpGasLimit = 
+            uint128(bytes16(userOp.accountGasLimits)) + // validation gas limit
+            uint128(uint256(userOp.accountGasLimits)) + // call gas limit
+            userOp.preVerificationGas; // pre verification gas
+
+        // paymasterAndData structure: 
+        // 20 bytes paymaster address
+        // 16 bytes paymasterVerificationGasLimit
+        // 16 bytes postOp gas limit
+        // extra data 
+        if (userOp.paymasterAndData.length >= 36) {
+            totalUserOpGasLimit += uint128(bytes16(userOp.paymasterAndData[20:36])); // paymasterVerificationGasLimit
         }
-        uint256 totalUserOpGasLimit = uint128(bytes16(userOp.accountGasLimits))
-            + uint128(uint256(userOp.accountGasLimits)) + userOp.preVerificationGas;
+        if (userOp.paymasterAndData.length >= 52) {
+            totalUserOpGasLimit += uint128(bytes16(userOp.paymasterAndData[36:52])); // postOp gas limit
+        }
+
         if (config.gasUsed + totalUserOpGasLimit > config.gasLimit) {
-            revert("GasLimitPolicy: gas limit exceeded");
+            return VALIDATION_FAILED;
         }
 
         // Limit will be quite accurate as per AA-217
@@ -35,6 +48,14 @@ contract SimpleGasPolicy is IUserOpPolicy {
     function initializeWithMultiplexer(address account, ConfigId configId, bytes calldata initData) external {
         gasLimitConfigs[configId][msg.sender][account].gasLimit = uint256(bytes32(initData[0:32]));
         gasLimitConfigs[configId][msg.sender][account].gasUsed = 0;
+    }
+
+    function getGasLimit(ConfigId configId, address multiplexer, address userOpSender) external view returns (uint256) {
+        return gasLimitConfigs[configId][multiplexer][userOpSender].gasLimit;
+    }
+
+    function getGasUsed(ConfigId configId, address multiplexer, address userOpSender) external view returns (uint256) {
+        return gasLimitConfigs[configId][multiplexer][userOpSender].gasUsed;
     }
 
     function supportsInterface(bytes4 interfaceID) external pure override returns (bool) {
