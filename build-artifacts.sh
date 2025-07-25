6bfe90643ae150578d7bb51a62b04ad4d74f6580
kopy-kat <kopp.konrad@gmail.com>
2025-07-25 11:04:08 +0100

    feat: init

--- build-artifacts.sh
+++ build-artifacts.sh
@@ -1,32 +1,16 @@
-
-
 #!/bin/bash
 
+# Check if a contract name is provided
+if [ $# -eq 0 ]; then
+    echo "Please provide a contract name as an argument."
+    echo "Usage: $0 <ContractPath>"
+    exit 1
+fi
 
+CONTRACT_PATH=$1
+CONTRACT_NAME=$(basename "$CONTRACT_PATH" .sol)
 
-mkdir -p ./artifacts/SmartSession
-forge build contracts/SmartSession.sol
-cp ./out/SmartSession.sol/* ./artifacts/SmartSession/.
-forge verify-contract --show-standard-json-input $(cast address-zero) ./contracts/SmartSession.sol:SmartSession > ./artifacts/SmartSession/verify.json
-
-
-
-mkdir -p ./artifacts/SudoPolicy
-forge build contracts/external/policies/SudoPolicy.sol
-cp ./out/SudoPolicy.sol/* ./artifacts/SudoPolicy/.
-forge verify-contract --show-standard-json-input $(cast address-zero) ./contracts/external/policies/SudoPolicy.sol:SudoPolicy > ./artifacts/SudoPolicy/verify.json
-
-mkdir -p ./artifacts/ERC20SpendingLimitPolicy
-forge build contracts/external/policies/ERC20SpendingLimitPolicy.sol
-cp ./out/ERC20SpendingLimitPolicy.sol/* ./artifacts/ERC20SpendingLimitPolicy/.
-forge verify-contract --show-standard-json-input $(cast address-zero) ./contracts/external/policies/ERC20SpendingLimitPolicy.sol:ERC20SpendingLimitPolicy > ./artifacts/ERC20SpendingLimitPolicy/verify.json
-
-
-mkdir -p ./artifacts/UniActionPolicy
-forge build contracts/external/policies/UniActionPolicy.sol
-cp ./out/UniActionPolicy.sol/* ./artifacts/UniActionPolicy/.
-forge verify-contract --show-standard-json-input $(cast address-zero) ./contracts/external/policies/UniActionPolicy.sol:UniActionPolicy > ./artifacts/UniActionPolicy/verify.json
-
-
-
-
+mkdir -p ./artifacts/$CONTRACT_NAME
+FOUNDRY_PROFILE=release forge build $CONTRACT_PATH
+cp ./out/$CONTRACT_NAME.sol/* ./artifacts/$CONTRACT_NAME/.
+forge verify-contract --show-standard-json-input $(cast address-zero) $CONTRACT_PATH:$CONTRACT_NAME > ./artifacts/$CONTRACT_NAME/verify.json
